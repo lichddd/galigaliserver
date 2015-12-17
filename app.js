f335a954eca9111052ff8f3f728d3bfae3ceb3d0
Your Name <lichddd@126.com>
2015-12-17 13:29:06 +0800

    1

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-
+var routes = require('./routes/index');
 var jsonp = require('./routes/jsonp');
 var app = express();
 
@@ -21,7 +21,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', jsonp);
+app.use('/', routes);
+app.use('/jsonp', jsonp);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
